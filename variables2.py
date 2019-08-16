8e65cbb291a0d1282b0532d44bfaeec98c1ad60a
Bijaya Sharma <33787531+vetbijaya@users.noreply.github.com>
2019-08-16 14:02:54 -0400

    Add files via upload

---
+++ variables2.py
@@ -0,0 +1,13 @@
+x = " awesome line 1"
+print("Python is"+ x)
+
+x = "Python is "
+y = "awesome line 2"
+z = x + y
+print(z)
+
+print("FOR NUMBERS + WORKS AS MATHEMATICAL OPERATORS")
+
+x = 10
+y = 65
+print(x + y)
